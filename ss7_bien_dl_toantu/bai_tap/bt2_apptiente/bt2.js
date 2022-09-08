800353ae72848ff06a8f8cdcfa2c30397ced51af
tyem123ks2 <thanhtuan02051011@gmail.com>
2022-09-08 16:54:34 +0700

    UPDATE

--- ss7_bien_dl_toantu/bai_tap/bt2_apptiente/bt2.js
+++ ss7_bien_dl_toantu/bai_tap/bt2_apptiente/bt2.js
@@ -1,14 +1,16 @@
 function CFA() {
-    let tienvao1 = document.getElementById("tienvao").value
-    let select1 = document.getElementById("select1").value
-    let select2 = document.getElementById("select2").value
-    if (select1 == select2) {
-        alert(tienvao1);
-    }
-    if (select1 == "VND" && select2 == "USD1") {
-        alert(tienvao1/23000);
-    }
-    if (select1 == "USD1" && select2 == "VND") {
-        alert(tienvao1*23000)
-    }
+    let amount = document.getElementById("tienvao").value;
+    let fromCurrency = document.getElementById("select1").value;
+    let toCurrency = document.getElementById("select2").value;
+    let result1;
+    if (fromCurrency == "VND" && toCurrency == "USD1") {
+        result1 = (amount / 23000) + " USD";
+    } else if (fromCurrency == "USD1" && toCurrency == "VND") {
+        result1 = (amount * 23000) + " VND";
+    } else if (fromCurrency == "VND" && toCurrency == "VND") {
+        result1 = amount + " VND";
+    } else if (fromCurrency == "USD1" && toCurrency == "USD1") {
+        result1 = amount + " USD";
+}
+        document.getElementById("result").innerText = result1;
 }
